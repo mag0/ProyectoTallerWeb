5a2dd16922fe7fc6754a4c2affd31d1365aad202
Sebastián Ch <schamorro@alumno.unlam.edu.ar>
2024-04-26 07:41:53 -0300

    implementacion contenido pedido
    se implementó la información de los pedidos y se hizo algunas correcciones

---
+++ src/main/java/com/tallerwebi/dominio/Pedidos.java
@@ -0,0 +1,58 @@
+package com.tallerwebi.dominio;
+
+public class Pedidos {
+    private String nombre;
+    private String tipo;
+    private String codigo;
+    private Integer tamanio;
+    private Integer peso;
+
+
+    public Pedidos(String nombre, String tipo, String codigo, Integer tamanio, Integer peso) {
+       this.nombre = nombre;
+        this.tipo = tipo;
+        this.codigo = codigo;
+        this.tamanio = tamanio;
+        this.peso = peso;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(Integer tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- src/main/java/com/tallerwebi/presentacion/ControladorPedidos.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPedidos.java
@@ -1,13 +1,37 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.Pedidos;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ControladorPedidos {
 
+    @RequestMapping("/pedidos")
+    public ModelAndView irAPedidos() {
+        List<Pedidos> pedido = new ArrayList<>();
+        pedido.add(new Pedidos("Paquete 1","Fragil", "DS225", 5, 10));
+        pedido.add(new Pedidos("Paquete 2","Contundente", "DX125", 3, 30));
+        pedido.add(new Pedidos("Paquete 3","Normal", "MS461", 10, 70));
+        pedido.add(new Pedidos("Paquete 4","Normal", "XXS345", 2, 9));
+        pedido.add(new Pedidos("Paquete 5","Fragil", "KJH764", 7, 45));
+
+        ModelMap modelMap = new ModelMap();
+        modelMap.addAttribute("pedido", pedido);
+        return new ModelAndView("pedidos", modelMap);
+
+    }
+}
+/*
     @GetMapping ("/pedidos")
         public String pedidos() {
              return "pedidos";
         }
 }
+*/
