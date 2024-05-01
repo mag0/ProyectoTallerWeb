b67ff48c5119e3d5705d8bc3d09cb78e22683a36
Lucas Barrios <lucabarrios@alumno.unlam.edu.ar>
2024-04-30 23:35:18 -0300

    vista de agregar vehiculo

--- src/main/java/com/tallerwebi/presentacion/ControladorRendimiento.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorRendimiento.java
@@ -1,2 +1,19 @@
-package com.tallerwebi.presentacion;public class ControladorRendimiento {
+package com.tallerwebi.presentacion;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorRendimiento {
+    @RequestMapping ("/rendimiento")
+    public ModelAndView irarendimiento(){
+        ModelMap model = new ModelMap();
+        return new ModelAndView("rendimiento", model);
+
+    }
+
+
 }
