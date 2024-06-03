52a1bd8a9af3d9f46e6e96297f9db3a918f8534c
martin <guerreiromartin@gmail.com>
2024-06-03 19:51:46 -0300

    mejora de la vista gestionVehicular

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,8 @@ INSERT INTO Pedido(nombre,tipo,codigo,tamanio,peso,fecha) VALUES  ("Paquete 2","
 INSERT INTO Pedido(nombre,tipo,codigo,tamanio,peso,fecha) VALUES  ("Paquete 3","Fragil", "DS227", 5, 10, "2021-01-01");
 INSERT INTO Pedido(nombre,tipo,codigo,tamanio,peso,fecha) VALUES  ("Paquete 4","Fragil", "DS228", 5, 10, "2021-01-01");
 INSERT INTO Pedido(nombre,tipo,codigo,tamanio,peso,fecha) VALUES  ("Paquete 5","Fragil", "DS229", 5, 10, "2021-01-01");
-INSERT INTO Vehiculo(marca,modelo,tipo,kilometrajeMaximo,combustible, resistencia,capacidad,patente,status) VALUES ("Fiat","Uno","Auto",100000,10000,1000,100,"ABC123",true);
+INSERT INTO Vehiculo(marca, modelo, tipo, kilometrajeMaximo, combustible, resistencia, capacidad, patente, status)
+VALUES
+    ("Fiat","Uno","Auto",100000,10000,1000,100,"ABC123",true),
+    ("Yamaha", "YZF-R3", "Moto", 30000, 5000, 800, 20, "MNO789", true),
+    ("Ford", "F-150", "Camion", 200000, 15000, 1500, 300, "JKL321", true);
