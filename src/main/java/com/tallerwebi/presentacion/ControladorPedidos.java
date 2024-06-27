9a737fb2634b98249719ab2c35fde2eae1cc45e6
Sebastián Ch <schamorro@alumno.unlam.edu.ar>
2024-06-27 09:22:21 -0300

    grupos por ubicacion ofertas

--- src/main/java/com/tallerwebi/presentacion/ControladorPedidos.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPedidos.java
@@ -19,6 +19,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class ControladorPedidos {
@@ -44,13 +46,19 @@ public class ControladorPedidos {
     public ModelAndView mostrarPedidos() {
         // Obtener todos los pedidos que no tienen viaje asignado
         List<Pedido> pedidos = pedidoService.getAllPedidosSinViaje();
-        // Crear un mapa de modelo y añadir los pedidos
+
+        // Agrupar los pedidos por estado y coordenadas
+        Map<String, List<Pedido>> pedidosAgrupados = pedidos.stream()
+                .collect(Collectors.groupingBy(p -> p.getLatitude() + "-" + p.getLongitude()));
+
+        // Crear un mapa de modelo y añadir los pedidos agrupados
         ModelMap modelMap = new ModelMap();
-        modelMap.addAttribute("pedidos", pedidos);
+        modelMap.addAttribute("pedidosAgrupados", pedidosAgrupados);
+
         // Retornar la vista 'ofertas' con el modelo
         return new ModelAndView("ofertas", modelMap);
-
     }
+
 
     @GetMapping("/ofertas/{id}/asignar")
     public ModelAndView asignarPedido(@PathVariable("id") Long id) {
